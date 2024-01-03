68d8ae1457704cceccec924f8c7a350a19c22f81
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-03 04:16:56 -0600

    added in error handling and adding pos checking

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -10,13 +10,20 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 	int l = 0, m, h;
+	int pos;
 
 	h = (int)size - 1;
+	if (array[h] < value)
+		return (-1);
 
 	while ((array[h] != array[l]) && (value >= array[l]) && (value <=array[h]))
 	{
 		m = l +(value - array[l]) * (h - l) / array[h] - array[l];
+		pos = l + (((double)(h - l) / (array[h] - array[l])) * (value - array[l]));
+		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
 
+		if (array[pos] == value)
+			return (pos);
 		if (array[m] < value)
 			l = m + 1;
 		else if (value < array[m])
@@ -24,4 +31,6 @@ int interpolation_search(int *array, size_t size, int value)
 		else
 			return (m);
 	}
+	
+	return (h);
 }
