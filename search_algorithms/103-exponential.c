fd0031cabb3422709daf00412ab7fe0a5df1bda1
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-12 01:38:35 -0600

    search

--- search_algorithms/103-exponential.c
+++ search_algorithms/103-exponential.c
@@ -1,8 +1,19 @@
 #include "search_algos.h"
 
-static int binarySearch(int arr[], int left, int right, int target)
+static int binary_search(int arr[], int left, int right, int target)
 {
-
+	while (left <= right)
+	{
+        int m = left + (right - left) / 2;
+        if (arr[m] == target)
+            return (m);
+ 
+        if (arr[m] < target)
+            left = m + 1;
+        else
+            right = m - 1;
+    }
+    return (-1);
 }
 
 int exponential_search(int *array, size_t size, int value)
@@ -16,5 +27,5 @@ int exponential_search(int *array, size_t size, int value)
 	{
 		bound *= 2;
 	}
-	return (binarySearch(array, bound/2, (((bound + 1) < (int)size) ? (bound + 1) : (int)size), value));
+	return (binary_search(array, bound/2, (((bound + 1) < (int)size) ? (bound + 1) : (int)size), value));
 }
