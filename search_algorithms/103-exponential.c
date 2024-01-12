d272d8d4c62d65c3730bd70a0d124fe0a64607f9
Taypo87 <taylor.poczynek87@gmail.com>
2024-01-12 10:32:17 -0600

    finding the index one iteration too soon

--- search_algorithms/103-exponential.c
+++ search_algorithms/103-exponential.c
@@ -1,12 +1,20 @@
 #include "search_algos.h"
 
+/**
+ * binary_search - binary search algorithm
+ * @array: array to be searched
+ * @left: the left boundry being searched
+ * @right: the right boundry being searched
+ * @target: the target value
+ * Return: the indice where the target was found, else -1
+*/
 static int binary_search(int array[], int left, int right, int target)
 {
 	int i = 0, m;
 
 	while (left <= right)
 	{
-        m = (left + (right - left) / 2);
+		m = (left + (right - left) / 2);
 		i = left;
 		printf("Searching in array: %d", array[i++]);
 		while (i <= right)
@@ -14,16 +22,21 @@ static int binary_search(int array[], int left, int right, int target)
 			printf(", %d", array[i++]);
 		}
 		printf("\n");
-        if (array[m] == target)
+		if (array[m] == target)
 			return (m);
-        if (array[m] < target)
-            left = m + 1;
-        else
-            right = m - 1;
-    }
-    return (-1);
+		if (array[m] < target)
+			left = m + 1;
+		else
+			right = m - 1;
+	}
+	return (-1);
 }
-
+/**
+ * exponential_search - exponential search algroithm
+ * @array: the array to be searched
+ * @size: the size of the array
+ * @value: the target value
+*/
 int exponential_search(int *array, size_t size, int value)
 {
 	int bound = 1, rebound = 0;
